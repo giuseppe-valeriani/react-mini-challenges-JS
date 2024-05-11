fb7f658b663a4f52d4a45b65aab9abba6e341c5f
Giuseppe Valeriani <giuggey85@gmail.com>
2024-05-11 10:22:43 +0100

    complete n1

--- src/solutions/Solution01.jsx
+++ src/solutions/Solution01.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
 const Solution01 = () => {
-  return <div>ADD YOUR SOLUTION HERE</div>;
+  const [number, setNumber] = useState(0);
+
+  const subtract = () => {
+    setNumber((prev) => prev - 1);
+  };
+
+  const add = () => {
+    setNumber((prev) => prev + 1);
+  };
+
+  return (
+    <main
+      style={{
+        display: "flex",
+        gap: "24px",
+        margin: "16px",
+        alignItems: "center",
+      }}
+    >
+      <button onClick={subtract} style={{ padding: "8px" }}>
+        -
+      </button>
+      <div style={{ fontSize: "24px" }}>{number}</div>
+      <button onClick={add} style={{ padding: "8px" }}>
+        +
+      </button>
+    </main>
+  );
 };
 
 export default Solution01;
